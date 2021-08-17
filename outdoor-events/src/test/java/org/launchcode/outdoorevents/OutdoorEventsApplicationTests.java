734ee0b0d385f72260227c58577e76e601f4363a
Thomas Hitschler <80077568+Hitz325@users.noreply.github.com>
2021-08-16 21:11:44 -0500

    Add files via upload

---
+++ outdoor-events/src/test/java/org/launchcode/outdoorevents/OutdoorEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.outdoorevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutdoorEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
